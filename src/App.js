8a77f284f46f9aa2827f991843b08159ded6c16f
ddsheard <ddsheard@gmail.com>
2017-04-25 19:56:28 -0400

    level 2

--- src/App.js
+++ src/App.js
@@ -27,6 +27,11 @@ componentDidMount () {
 
 focus() {
   this.textInput.focus();
+  // console.log(this.textInput.value);
+  axios.get('https://api.github.com/users/' + this.textInput.value)
+  .then (response => {
+    this.setState({user: response.data})
+  })
 }
 // +this.state.user.login
 
@@ -37,17 +42,27 @@ focus() {
 
     return (
       <div className="GithubApp">
-        <input type="text"  ref={(input) => {this.textInput = input;}}/>
-        <input  type="button" value="select github user" onClick={this.focus}/>
-        <h1>{this.state.user.name}</h1>
-        <p>{this.state.user.login}</p>
-        <img src={this.state.user.avatar_url} alt="github user image"/>
-        <br />
-        <a href={this.state.user.html_url} target="_blank">Visit my github repository</a>
-        <br />
-        <a href={this.state.user.blog}>Blog</a>
-        <p>Location: {this.state.user.location}</p>
-        <p>{this.state.user.hireable ? "Hireable: Yes" : "Hireable: No"}</p>
+        <div className="input">
+          <input
+            type="text"
+            ref={(input) => {this.textInput = input;}}/>
+          <input
+            type="button"
+            value="select github user"
+            onClick={this.focus.bind(this)}
+          />
+        </div>
+        <div>
+          <h1>{this.state.user.name}</h1>
+          <p>{this.state.user.login}</p>
+          <img src={this.state.user.avatar_url} alt="github user image"/>
+          <br />
+          <a href={this.state.user.html_url} target="_blank">Visit my github repository</a>
+          <br />
+          <a href={this.state.user.blog}>Blog</a>
+          <p>Location: {this.state.user.location}</p>
+          <p>{this.state.user.hireable ? "Hireable: Yes" : "Hireable: No"}</p>
+        </div>
       </div>
     );
   // } else {
